a23a1a5de1390f751f2370de917445ba5d23cea1
Marinade <marinadegames@gmail.com>
2022-02-07 21:50:37 +0300

    added form

--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -1,6 +1,8 @@
 // imports
 import React from "react";
 import s from './Contacts.module.css'
+import {UniversalButton} from "../UniversalButton/UniversalButton";
+import {UniversalButtonSecond} from "../UniversalButtonSecond/UniversalButtonSecond";
 
 // types
 
@@ -11,12 +13,14 @@ export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
             <div className={s.contactsBlockContainer}>
-                <h2>Contacts:</h2>
-                <form>
-                    <input/>
-                    <input/>
-                    <input/>
+                <h2>Write me</h2>
+                <form className={s.formContacts}>
+                    <input type={'text'} placeholder={'Name'}/>
+                    <input type={'text'} placeholder={'Email'}/>
+                    <textarea placeholder={'Message'}/>
                 </form>
+
+                <UniversalButtonSecond title={'send!'}/>
             </div>
         </div>
     )
