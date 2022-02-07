522bf84d82cc0c318e874aed5b5dba16a8edc4e9
Marinade <marinadegames@gmail.com>
2022-02-07 21:24:40 +0300

    added BURGER

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,24 +1,38 @@
 // imports
-import React from "react";
+import React, {useState} from "react";
 import s from './Header.module.css'
 import {Nav} from "../Nav/Nav";
 import {UniversalButton} from "../UniversalButton/UniversalButton";
 import {Logo} from "../Logo/Logo";
+import {NavBurger} from "../NavBurger/NavBurger";
 
 
 // component
 export const Header = () => {
+
+    const [openNav, setOpenNav] = useState<boolean>(false)
+
+    const onClickMenuHandler = () => {
+        setOpenNav(!openNav)
+    }
+
     return (
-        <div className={s.Header}>
-            <div className={s.HeaderDesktop}>
-                <Logo/>
-                <Nav/>
-                <UniversalButton title={'email'}/>
+        <>
+            <div className={s.Header}>
+                <div className={s.HeaderDesktop}>
+                    <Logo/>
+                    <Nav/>
+                    <UniversalButton title={'email'}/>
+                </div>
+                <div className={s.HeaderMobile}>
+                    <button className={s.burgerMenu} onClick={onClickMenuHandler}>menu</button>
+
+                    <Logo/>
+
+                </div>
+
             </div>
-            <div className={s.HeaderMobile}>
-                <button className={s.burgerMenu}>menu</button>
-                <Logo/>
-            </div>
-        </div>
+            {openNav ? <NavBurger/> : ''}
+        </>
     )
 }
