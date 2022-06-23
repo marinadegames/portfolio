cc8a164998d07d91f45907b6a4b5f10195a09ae4
marinadedev <marinadegames@gmail.com>
2022-06-23 23:04:26 +0300

    * created Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './App.module.css'
+import {Header} from "./Components/Header/Header";
 
 export const App = () => {
     return (
         <div className={s.App}>
-            HELLO!
+            <Header/>
         </div>
     );
 }
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import s from './Header.module.css';
+
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <h1>Header</h1>
+            <h1>Header</h1>
+            <h1>Header</h1>
+        </div>
+    )
+}
